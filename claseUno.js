30460def65439c7799a3f8b8422d04f3803a7705
Julian Gareca Kandrachoff <jugakan@gmail.com>
2021-06-24 20:50:32 -0300

    buenas practicas!

--- claseUno.js
+++ claseUno.js
@@ -16,24 +16,24 @@
 const unNumero = parseInt(prompt("ingresar un Numero!!"))
 
     if (unNumero >= 1000) {
-        alert ("Buenisimo! adivinaste!")
+        alert ("Buenisimo! adivinaste!");
     }
 
 const unaPalabra = prompt("Saludame, pero como la REA lo haria")
 
     if (unaPalabra === "Hola") {
-        console.log ("Buenisimo! adivinaste de nuevo!")
+        console.log ("Buenisimo! adivinaste de nuevo!");
     }
 
 const otroNumero= parseInt(prompt("ahora ingresa oooooooooootro número pero entre 0 y 100"))
 
     if ((otroNumero >=10) && (otroNumero <=50)){
-        alert ("Grande Campeon!! pero no tanto como bokita")
+        alert ("Grande Campeon!! pero no tanto como bokita");
     }
     else if (otroNumero <10){
-        alert ("no negro, es muy chico")
+        alert ("no negro, es muy chico");
     }
     else if (otroNumero >50){
-        alert ("Fua, ni messi se animo a tanto")
+        alert ("Fua, ni messi se animo a tanto");
     }
 
