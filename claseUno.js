30648b06f9c76016a6e67ee1c2b534dc4dcb55d1
Julian Gareca Kandrachoff <jugakan@gmail.com>
2021-06-24 20:32:23 -0300

    Clase 2

---
+++ claseUno.js
@@ -0,0 +1,39 @@
+//  const miNombre = "Julian";
+//  const apellido = "Gareca Kandrachoff";
+//  const espacio = " ";
+//  const concatenacion = miNombre + " " + apellido + espacio;
+
+//  console.log(concatenacion);
+
+
+// const numeroTelefono = parseInt(prompt("ingrese su número de telefono"));
+// alert("Usted ha ingresado: " + numeroTelefono);
+// const compania = prompt("ingrese su compañia de teléfono");
+// alert("Gracias por visitar nuestra pagina! =) mi nombre es " + miNombre);
+
+/* clase 1!*/
+
+const unNumero = parseInt(prompt("ingresar un Numero!!"))
+
+    if (unNumero >= 1000) {
+        alert ("Buenisimo! adivinaste!")
+    }
+
+const unaPalabra = prompt("Saludame, pero como la REA lo haria")
+
+    if (unaPalabra === "Hola") {
+        console.log ("Buenisimo! adivinaste de nuevo!")
+    }
+
+const otroNumero= parseInt(prompt("ahora ingresa oooooooooootro número pero entre 0 y 100"))
+
+    if ((otroNumero >=10) && (otroNumero <=50)){
+        alert ("Grande Campeon!! pero no tanto como bokita")
+    }
+    else if (otroNumero <10){
+        alert ("no negro, es muy chico")
+    }
+    else if (otroNumero >50){
+        alert ("Fua, ni messi se animo a tanto")
+    }
+
