8e5fd44d6b0d734186e20b540af8369c2d7bf02d
Julian Gareca Kandrachoff <jugakan@gmail.com>
2021-06-28 20:15:23 -0300

    Clase numero 3

--- claseUno.js
+++ claseUno.js
@@ -1,3 +1,5 @@
+/*CLASE Nº 1 */
+
 //  const miNombre = "Julian";
 //  const apellido = "Gareca Kandrachoff";
 //  const espacio = " ";
@@ -11,29 +13,43 @@
 // const compania = prompt("ingrese su compañia de teléfono");
 // alert("Gracias por visitar nuestra pagina! =) mi nombre es " + miNombre);
 
-/* clase 1!*/
+/* clase 2!*/
 
-const unNumero = parseInt(prompt("ingresar un Numero!!"))
+// const unNumero = parseInt(prompt("ingresar un Numero!!"))
 
-    if (unNumero >= 1000) {
-        alert ("Buenisimo! adivinaste!");
-    }
+//     if (unNumero >= 1000) {
+//         alert ("Buenisimo! adivinaste!");
+//     }
 
-const unaPalabra = prompt("Saludame, pero como la REA lo haria")
+// const unaPalabra = prompt("Saludame, pero como la REA lo haria")
 
-    if (unaPalabra === "Hola") {
-        console.log ("Buenisimo! adivinaste de nuevo!");
-    }
+//     if (unaPalabra === "Hola") {
+//         console.log ("Buenisimo! adivinaste de nuevo!");
+//     }
 
-const otroNumero= parseInt(prompt("ahora ingresa oooooooooootro número pero entre 0 y 100"))
+// const otroNumero= parseInt(prompt("ahora ingresa oooooooooootro número pero entre 0 y 100"))
 
-    if ((otroNumero >=10) && (otroNumero <=50)){
-        alert ("Grande Campeon!! pero no tanto como bokita");
-    }
-    else if (otroNumero <10){
-        alert ("no negro, es muy chico");
-    }
-    else if (otroNumero >50){
-        alert ("Fua, ni messi se animo a tanto");
-    }
+//     if ((otroNumero >=10) && (otroNumero <=50)){
+//         alert ("Grande Campeon!! pero no tanto como bokita");
+//     }
+//     else if (otroNumero <10){
+//         alert ("no negro, es muy chico");
+//     }
+//     else if (otroNumero >50){
+//         alert ("Fua, ni messi se animo a tanto");
+//     }
 
+/* clase 3 */ 
+
+let ingresarNumero = Number(prompt ("Ingresar numero"))
+
+for (let i = 1; i <= 23; i++) {
+    let resultado = ingresarNumero + i;
+    console.log(resultado);
+}
+
+let entrada = prompt("Ingresar un dato");
+while(entrada != "ESC" ){
+    console.log("El usuario ingresó "+ entrada);
+    entrada = prompt("Ingresar otro dato");
+}
