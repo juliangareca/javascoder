396fbbfb40cee35d613cdad713a96025b278bda1
Julian Gareca Kandrachoff <jugakan@gmail.com>
2021-07-27 19:30:16 -0300

    Clase 8

---
+++ prueba.js
@@ -0,0 +1,10 @@
+let numero1 = Number(prompt("ingresar edad"));
+let numero2 = Number(prompt("ingresar antiguedad"));
+
+let multi = numero1 * numero2;
+
+if (multi >= 100) {
+  console.log(`Bien ahi`);
+} else if (multi < 100) {
+  console.log(`mal ahi`);
+}
